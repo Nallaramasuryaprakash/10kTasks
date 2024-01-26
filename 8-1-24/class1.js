12913240abdb0dc3fc51646918fef6f8050f232a
Nallaramasuryaprakash <nallaramasuryaprakash@gmail.com>
2024-01-26 10:29:37 +0530

    pop and shift

---
+++ 8-1-24/class1.js
@@ -0,0 +1,13 @@
+// pop
+function array(a)
+{
+var a;
+b=[];
+for(i=0;i<a.length-1;i++)
+{
+    b.push(a[i])
+}
+console.log(b);
+}
+
+array([1,2,3])
---
+++ 8-1-24/class2.js
@@ -0,0 +1,13 @@
+// shift
+function array(a)
+{
+var a;
+b=[];
+for(i=1;i<a.length;i++)
+{
+    b.push(a[i])
+}
+console.log(b);
+}
+
+array([1,2,3])
